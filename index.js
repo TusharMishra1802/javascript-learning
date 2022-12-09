2099e137d15939cbe5fdafb03f690f2f91f66e32
tusharmishrawork <tusharmishrawork@gmail.com>
2022-12-09 23:16:50 +0530

    Javascript fundamental

---
+++ index.js
@@ -0,0 +1,74 @@
+// ----------------> JavaScript Fundamentals <---------------------  
+
+// Hello Word 
+// console.log('Hello World');
+// document.write('Hello World');
+
+// use strict --> this is use for write code in strict mode
+
+// variables --> let , const , var
+// let variable1; 
+// const variable2;
+// var variable3;
+
+// DataTypes --> String,Number,Boolean,null,undefined,Object,symbol,BigInt
+// let str = 'StringType';
+// let num = 123456;
+// let bol = true;
+// let nul = null;
+// let undef;
+// let obj = {
+//     key:value
+// }
+
+// alert,prompt,confirm
+// alert('This is alert'); --> this gives a alert on browser
+// prompt('this is prompt'); --> this gives you a prompt and an input field
+// confirm('this is confirm') --> this gives you yes and no confirmation
+
+// Type-Conversion
+// String(),Number(),Boolean()
+
+// Basic Operators
+// + - / * % ** || && ! ++ --
+
+// Condtional Statement
+// if,if-else,if-else if-else 
+// codntion ? statement1 : statement2
+
+// Logical operators
+// || && !
+
+// Nullish coalescing operator '??'
+// ?? returns the first argument if it’s not null/undefined. Otherwise, the second one.
+
+// Loops --> while,do-while,for,for-in,for-of
+
+// Switch Statement 
+// switch(){
+//     case:1
+//     // statement
+//     break;
+//     .
+//     .
+//     .
+//     default:
+//         break
+
+// }
+
+
+//Function
+// function Check(parameters){
+// statements
+// } 
+// Check()
+
+
+//Arrow Function
+//  const check = ()=>{
+//     statement
+//  }
+//  check()
+
+// ----------------> JavaScript Fundamentals <---------------------  
